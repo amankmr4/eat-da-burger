4429c20e8c6b9ea903e16f363cb3a09ce9b2cc56
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-12 23:56:13 +1100

    added mysql database schema and seed file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burger (burger)
+VALUES ('Fried Chicken');
+INSERT INTO burger (burger)
+VALUES ('Double Stacker');
+INSERT INTO burger (burger, devoured)
+VALUES ('Zinger Stacker', true);
+INSERT INTO burger (burger, devoured)
+VALUES ('McChiken', true);
+INSERT INTO burger (burger, devoured)
+VALUES ('Triple Bondi', true);
+INSERT INTO burger (burger)
+VALUES ('Double Cheese Burger');
