d6c266232a6edb94e1f06e8efeac6786f0c0d5d8
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-13 00:56:06 +1100

    added the orm fucntions to be called in my models

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
