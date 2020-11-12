686dd3972037b14a8af581f6abbe63956211ec30
Aman Kumar <aman.kmr4@yahoo.com>
2020-11-13 01:01:34 +1100

    added my burger.js to create fucntions using orm

--- models/burger.js
+++ models/burger.js
@@ -2,27 +2,22 @@ var orm = require("../config/orm.js");
 
 var cat = {
     all: function (cb) {
-        orm.all("cats", function (res) {
+        orm.all("burger", function (res) {
             cb(res);
         });
     },
     // The variables cols and vals are arrays.
     create: function (cols, vals, cb) {
-        orm.create("cats", cols, vals, function (res) {
+        orm.create("burger", cols, vals, function (res) {
             cb(res);
         });
     },
     update: function (objColVals, condition, cb) {
-        orm.update("cats", objColVals, condition, function (res) {
-            cb(res);
-        });
-    },
-    delete: function (condition, cb) {
-        orm.delete("cats", condition, function (res) {
+        orm.update("burger", objColVals, condition, function (res) {
             cb(res);
         });
     }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgerController.js).
 module.exports = cat;
